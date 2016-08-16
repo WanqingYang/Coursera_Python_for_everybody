2a5b5672af7659b8554145433819a6c7aac3422f
WanqingYang <latejean@gmail.com>
2016-08-16 15:10:30 -0700

    Update 10.2.py

--- 10.2.py
+++ 10.2.py
@@ -3,3 +3,17 @@
 #You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
 #From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 #Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+#use dictionary to count
+counts = dict()
+for line in handle:
+  if line.startswith('From') and not line.startswith('From:'):#if without'and not' index will go out of range
+    word = line.split()
+    counts[word[5][0:2]] = counts.get(word[5][0:2], 0) + 1
+    
+for k, v in sorted(counts.items()):
+  print k, v
